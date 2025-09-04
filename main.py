6691a71a6f867427d4936362e0a17bd999afb5fb
nadimelsalah-cmyk <nadimelsalah@gmail.com>
2025-09-04 10:48:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 import sqlite3
 import telegram
 import asyncio
+import os
 
-# Telegram bot credentials
-bot_token = '8041558734:AAGkbIaBHTzKLaQoDgtuiCInnwCX7y2wImo'  # Replace with your bot token
-bot_chat_id = '-1002166281411'  # Replace with your chat ID
+bot_token = os.getenv("BOT_TOKEN")
+bot_chat_id = os.getenv("CHAT_ID")
 
-# Create a bot instance
 bot = telegram.Bot(token=bot_token)
 
-# Connect to SQLite database
 def create_connection():
     conn = sqlite3.connect('sent_articles.db')
     return conn
 
-# Create a table for sent articles if it doesn't exist
 def create_table():
     conn = create_connection()
     with conn:
@@ -26,7 +23,6 @@ def create_table():
         ''')
     conn.close()
 
-# Check if the article has already been sent
 def is_article_sent(link):
     conn = create_connection()
     cursor = conn.cursor()
@@ -35,7 +31,6 @@ def is_article_sent(link):
     conn.close()
     return exists
 
-# Mark an article as sent
 def mark_article_as_sent(link):
     conn = create_connection()
     with conn:
@@ -47,3 +42,4 @@ async def send_message_to_telegram_channel(message):
 
 def init_database():
     create_table()
+
